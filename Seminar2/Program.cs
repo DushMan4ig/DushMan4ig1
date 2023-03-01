2197a23640a1cf2d3dffbd5ea8eb66f89e401385
Sedyh Valerii <hameleon90@list.ru>
2023-03-01 22:11:49 +0600

    Все задания сделаны

--- Seminar2/Program.cs
+++ Seminar2/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿// Программа:
+
+// 1 На вход принимает число.
+// 2 Выдает его квадрат.
+
+// Например: 
+// 4 -> 16
+// -3 -> 9
+// -7 -> 49
+
+Console.WriteLine("Введите целое число");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+int square = number * number;
+
+Console.WriteLine($"Квадрат числа {number} = {square}");
+
+
