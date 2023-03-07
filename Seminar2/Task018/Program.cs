dd55a93005cfdc57259c785b32c4e52f106c763c
Sedyh Valerii <hameleon90@list.ru>
2023-03-07 20:48:33 +0600

    Сделаны задачи 19 21 23

---
+++ Seminar2/Task018/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine($"Введите номер четверти");
+Console.Write("Четверть:");
+string quarter = Console.ReadLine();
+
+string range = FindRange (quarter);
+Console.WriteLine(range);
+
+string FindRange (string quart)
+{
+    if(quart == "1") return "x > 0 и y > 0";
+    if(quart == "2") return "x < 0 и y > 0";
+    if(quart == "3") return "x < 0 и y < 0";
+    if(quart == "4") return "x > 0 и y < 0";
+    return "Не соотвествует никакой четверти";
+    
+}
---
+++ Seminar2/Task022/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите натуральное число : ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Table(number);
+void Table(int num)
+{
+    int index = 1;
+    int result = 0;
+    while (index <= num)
+    {
+        result = index * index;
+        Console.WriteLine($"{index, 5} {result, 5}");
+        index++;
+    }
+}
