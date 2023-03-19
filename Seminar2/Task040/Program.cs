070f03cf12e0018e38995aefd47b4a3cb95f7306
Sedyh Valerii <hameleon90@list.ru>
2023-03-19 11:49:41 +0600

    Сделаный задачи 41 43

---
+++ Seminar2/Task040/Program.cs
@@ -0,0 +1,14 @@
+﻿bool IsExistTriangle(int a, int b, int c)
+{
+    if (a + b > c && b + c > a && c + a > b) return true;
+    else return false;
+}
+Console.WriteLine("Введите длину стороны A: ");
+int sideA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите длину стороны B: ");
+int sideB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите длину стороны C: ");
+int sideC = Convert.ToInt32(Console.ReadLine());
+string output = IsExistTriangle(sideA, sideB, sideC) ? "Треугольник может существовать с такими сторонами" : 
+"Треугольник не может сущетсвовать с такими сторонами";
+Console.WriteLine(output);
---
+++ Seminar2/Task044/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Число N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+
+int[] FibonacciArray(int num)
+{
+    int[] array = new int[number]; // Задаем размер массива
+    array[0] = 0;
+    array[1] = 1;
+    for (int i =2 ; i < num; i++)    
+    {
+        array[i] = array[i - 1] + array[i - 2];
+    }
+    return array;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]},");
+    }
+    Console.WriteLine("]");
+}
+
+int[] fiboArr = FibonacciArray(number); // Вызываем заданный массив
+PrintArray(fiboArr);
